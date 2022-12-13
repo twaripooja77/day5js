9ca9b99ceccd99bc95a8f96ed2a17c705a0062ff
Pooja Tiwari <twaripooja77@gmail.com>
2022-12-13 17:46:42 +0545

    basic js

---
+++ day5.js
@@ -0,0 +1,64 @@
+// single line comment
+// //declaring variables
+// var a=1; //global scope
+// let b=2;  //can reassign but cannot be redeclared
+// //value cannot be changed and cannot be redeclared
+// const c=5;
+// alert(a+b);
+
+let a=10;
+let name="pooja";
+let test; //undefined
+//  prompt(test);
+console.log("hello I am test");
+
+let b=5;
+// alert(a+b);
+
+// let c=a+b;
+// console.log(c);
+// let d=c*a;
+// console.log(d);
+// alert(a==b);
+
+//double equals is used to check the value only
+//triple equals is used to check the value and datatypes
+
+let isloggedIn= true;
+let t= isloggedIn && true;
+// alert(t);
+let superuser=false;
+
+let x=isloggedIn || superuser;
+// alert(x);
+
+// if else
+// 
+
+
+// //ARRAY
+// let array=[1,2,3,4,5,6,7];
+// alert(array[2]);
+//console.log(array,"array");
+
+
+//objects
+let object1={
+    name:"pooja",
+    age:23,
+}
+console.log(object1.name);
+console.log(object1,"object1");
+console.log(object1["age"],"object1");
+
+let object2={
+    name1:"banana",
+    name2:"mango",
+    name3:"strawberry",
+}
+console.log(object2);
+
+
+
+
+
